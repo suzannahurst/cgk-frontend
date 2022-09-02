bd10b462bd69747a59475f61eec5ee00b4e9e497
suzannahurst <hurstsuzanna@gmail.com>
2022-09-02 15:29:14 +0100

    anim

--- scripts/landing.js
+++ scripts/landing.js
@@ -8,17 +8,20 @@ landing.addEventListener("click", () => {
     duration: 5,
     scale: 8,
     opacity: 0,
+    ease: "power1.out",
   });
   gsap.to(".scene", {
     delay: 0.5,
     duration: 9,
     scale: 1,
     opacity: 1,
+    ease: "power1.out",
   });
   gsap.to(".info-wrapper", {
-    delay: 1.5,
+    delay: 2.5,
     duration: 1.5,
     backgroundColor: "#EA9E25",
     border: " #fff 1px solid",
+    ease: "power1.out",
   });
 });
