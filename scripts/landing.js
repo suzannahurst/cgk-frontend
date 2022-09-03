8a6e7a371c35831736fc9bc4a2e9add5bdf94855
suzannahurst <hurstsuzanna@gmail.com>
2022-09-03 13:51:20 +0100

    adjust anim

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,27 +1,31 @@
 const landing = document.getElementById("landing");
 const scene = document.getElementById("scene");
+const overlay = document.getElementById("overlay");
 
 landing.addEventListener("click", () => {
-  // scene.addClass("show");
   gsap.to(".black", {
     y: -400,
     duration: 5,
     scale: 8,
     opacity: 0,
-    ease: "power1.out",
+    // ease: "power1.out",
   });
-  gsap.to(".scene", {
-    delay: 0.5,
-    duration: 9,
-    scale: 5,
-    opacity: 1,
-    ease: "power1.out",
-  });
+  overlay.classList.add("fade-in");
+  // gsap.to(".scene", {
+  //   delay: 0.5,
+  //   duration: 5,
+  //   scale: 1.2,
+  //   opacity: 1,
+  //   // ease: "power1.out",
+  // });
+
+  setTimeout(overlay.classList.add("reveal"), 5000);
+
   gsap.to(".info-wrapper", {
-    delay: 2.5,
-    duration: 1.5,
+    // delay: 1,
+    // duration: 4,
     backgroundColor: "#EA9E25",
     border: " #fff 1px solid",
-    ease: "power1.out",
+    //   ease: "power1.out",
   });
 });
