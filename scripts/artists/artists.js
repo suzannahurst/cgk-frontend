d2e6abc2444af93484b02628c8ae28649c9c8ac3
suzannahurst <hurstsuzanna@gmail.com>
2022-09-04 11:34:09 +0100

    artist images dynamic

--- scripts/artists/artists.js
+++ scripts/artists/artists.js
@@ -58,42 +58,36 @@ export const displayArtist = (artist) => {
     slug,
     tags,
     showlocation,
+    image,
   } = artist.data.attributes;
   const fairName = fair.data.attributes.name;
   const fairSlug = fair.data.attributes.slug;
   const fairGm = fair.data.attributes.gm;
   console.log(artist.data.attributes);
-  console.log(fair.data.attributes);
+  // console.log(fair.data.attributes);
   // TODO check why this [0] is different to another collection
 
   const showInfoMount = document.getElementById("showInfoMount");
-  // showInfoMount.innerHTML = `<h2 class="title" id="artistTitle">${name}</h2>
-  //     <img id="artistImg" class="artist-img hero" src=https://coney-golden-key.herokuapp.com/api/${artist.img[0].url}>
-
-  //     <h3 class="subtitle" id="showName">${showname}</h3>
-  //     <p id="showDescription">${description}</p>
-  //     <p>Times: <span id="showTimes">${shows}</p>
-  //     <p>Fair: <span id="artistFair"><a href="./../fairs/${fair.slug}.html" class="highlighted">${fair.title}</a> </span>
-  //     </p>`;
+  const artistInfoMount = document.getElementById("artistInfo");
+  const tagsMount = document.getElementById("tagsMount");
+  const imgMount = document.getElementById("artistImgMount");
+  const fairMount = document.getElementById("fairMount");
 
   showInfoMount.innerHTML = `
       <h3 class="subtitle" id="showName">${showname}</h3>
       <p>Times: <span >${showtimes}</span></p>
-      <p>Location: <span><a href="${fairGm}" target="_blank" class="highlighted">${showlocation} </a></span></p>
+      <p class="maintext">Location: <span><a href="${fairGm}" target="_blank" class="highlighted">${showlocation} </a></span></p>
       <p>Fair: <span><a href="./../fairs/${fairSlug}.html" class="highlighted">${fairName}</a> </span>
       </p>
-      <p id="showDescription">${description}</p>
+      <p id="showDescription" class="maintext">${description}</p>
       `;
 
-  const artistInfoMount = document.getElementById("artistInfo");
   artistInfoMount.innerHTML = ` <h3 class="subtitle">About <span id="aboutArtistTitle">${name}</span></h3>
-      <p id="artistBiog">${biog}</p>`;
+      <p id="artistBiog" class="maintext">${biog}</p>`;
   // add if we implement artist tags
 
-  const tagsMount = document.getElementById("tagsMount");
-
   let tag;
-  console.log(tags);
+  // console.log(tags);
 
   tags.data.map((tg) => {
     console.log(tg);
@@ -105,9 +99,14 @@ export const displayArtist = (artist) => {
       tagsMount.appendChild(tag);
     }
   });
-  const fairMount = document.getElementById("fairMount");
+
   fairMount.innerHTML = ` <p>Back to <span><a href="./../fairs/${fairSlug}.html" class="highlighted">${fairName}</a>. </span>
       </p>`;
+
+  const imgUrl = image.data.map((img) => {
+    return img.attributes.url;
+  });
+  imgMount.innerHTML = `<img id="artistImg" class="artist-img" src=${imgUrl}></img>`;
 };
 
 // const showdown = window.showdown;
