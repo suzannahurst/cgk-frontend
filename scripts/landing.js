e67bc482b6af9a1c73f03e33f2949c270043a5e4
suzannahurst <hurstsuzanna@gmail.com>
2022-09-04 16:55:44 +0100

    opening anim

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,33 @@
 const landing = document.getElementById("landing");
-const scene = document.getElementById("scene");
+const keyhole = document.getElementById("keyholeBlack");
 const overlay = document.getElementById("overlay");
 
 landing.addEventListener("click", () => {
   gsap.to(".black", {
     y: -400,
-    duration: 5,
+    duration: 4,
     scale: 8,
     opacity: 0,
     // ease: "power1.out",
   });
-  overlay.classList.add("fade-in");
-  // gsap.to(".scene", {
-  //   delay: 0.5,
-  //   duration: 5,
-  //   scale: 1.2,
-  //   opacity: 1,
-  //   // ease: "power1.out",
-  // });
+  // overlay.classList.add("fade-in");
+  gsap.to(".overlay", {
+    delay: 1,
+    duration: 5,
+    scale: 1,
+    opacity: 1,
+    //   // ease: "power1.out",
+  });
 
-  setTimeout(overlay.classList.add("reveal"), 5000);
+  setInterval(() => {
+    keyhole.classList.add("hide");
+  }, 5000);
 
   gsap.to(".info-wrapper", {
-    // delay: 1,
-    // duration: 4,
-    backgroundColor: "#EA9E25",
+    delay: 1,
+    duration: 4,
+    backgroundColor: "#e86530",
     border: " #fff 1px solid",
-    //   ease: "power1.out",
+    ease: "power1.out",
   });
 });
