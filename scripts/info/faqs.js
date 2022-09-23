b67653d783ca533086ca7b59df9728bd866883ac
suzannahurst <hurstsuzanna@gmail.com>
2022-09-23 18:06:36 +0100

    order of faqs

--- scripts/info/faqs.js
+++ scripts/info/faqs.js
@@ -10,12 +10,35 @@ export const fetchFaqs = () => {
   fetch(faqsReq)
     .then((response) => response.json())
     .then((faqs) => {
-      let faqList = document.getElementById("faqList");
+      // let faqList = document.getElementById("faqList");
+      let aboutList = document.getElementById("aboutList");
+      let travelList = document.getElementById("travelList");
+      let planList = document.getElementById("planList");
+      let securityList = document.getElementById("securityList");
+      let localList = document.getElementById("localList");
       let data = faqs.data;
       data.map((faq) => {
-        faqList.appendChild(displayFaq(faq));
+        switch (faq.attributes.category) {
+          case "about":
+            aboutList.appendChild(displayFaq(faq));
+            break;
+          case "travel":
+            travelList.appendChild(displayFaq(faq));
+            break;
+          case "plan":
+            planList.appendChild(displayFaq(faq));
+            break;
+          case "security":
+            securityList.appendChild(displayFaq(faq));
+            break;
+          case "local":
+            localList.appendChild(displayFaq(faq));
+            break;
+          default:
+            aboutList.appendChild(displayFaq(faq));
+        }
       });
-      console.log(faqs);
+      // console.log(faqs);
       return faqs;
     })
     .catch((error) => {
@@ -25,13 +48,11 @@ export const fetchFaqs = () => {
 
 export const displayFaq = (faq) => {
   const { question, answer } = faq.attributes;
-  console.log(faq.attributes);
+
   const li = document.createElement("li");
   li.classList.add("faqItem", "listItem");
   // li.innerHTML =
-  li.innerHTML = `<h3 class="subtitle">${question} </h3>${converter.makeHtml(
-    answer,
-  )}
+  li.innerHTML = `<h3 class="">${question} </h3>${converter.makeHtml(answer)}
 `;
 
   return li;
